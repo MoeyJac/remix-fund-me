70f3092c1a618a078095a2dcfde39e8429495a54
Moeyjac <jo3ymac@gmail.com>
2023-05-31 10:26:09 -0400

    feat: moved price conversion logic to library

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,37 +2,21 @@
 
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
     uint256 public minimumUSD = 50 * 1e18;
+
+    address[] funders;
+    mapping(address => uint256) public addressToAmountFunded;
     
     function fund() public payable {
         // Want to be able to specify a minimum amount for deposit
-        // 1. How do we send ETH to this address
-
-         require(getConversionRate(msg.value) >= minimumUSD, "Didnt meet minimum deposit amount");
-    }
-
-    function getLatestPrice() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int price,,,) = priceFeed.latestRoundData();
-
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
-        uint256 ethPrice = getLatestPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256){
-        // Sepolia contract address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-
-
+         require(msg.value.getConversionRate() >= minimumUSD, "Didnt meet minimum deposit amount");
+         funders.push(msg.sender);
+         addressToAmountFunded[msg.sender] += msg.value;
     }
 
     // functiion withdraw() public {}
