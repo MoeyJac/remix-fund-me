2a2cffa2430d617c851e2f381497a4930fe551f6
Moeyjac <jo3ymac@gmail.com>
2023-05-31 11:20:37 -0400

    feat: added functionality for fund and withdraw function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -12,13 +12,37 @@ contract FundMe {
     address[] funders;
     mapping(address => uint256) public addressToAmountFunded;
     
-    function fund() public payable {
+    function fund() external payable {
         // Want to be able to specify a minimum amount for deposit
          require(msg.value.getConversionRate() >= minimumUSD, "Didnt meet minimum deposit amount");
          funders.push(msg.sender);
          addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    // functiion withdraw() public {}
+    function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            // Reset funder amount to 0
+            addressToAmountFunded[funder] = 0;
+        }
+        // Reset the funders list
+        funders = new address[](0);
+
+        // Withdraw funds
+
+        // transfer - reverts if it fails automatically
+        // msg.sender = address
+        // payable(msg.sender) = payable address
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send - returns bool if success/fail. Does NOT fail automatically
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+
+        // call - can call a function and return success + data from called function
+        // most used function to transfer ethereum 
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
 
 }
